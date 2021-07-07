d37391f632b91c9be79eba5596ce94ee22b7565c
abdoliloaf <abdoliloaf@gmail.com>
2021-07-07 11:47:42 +0100

    this m-clientui service

---
+++ src/main/java/com/clientui/clientui/exceptions/CustomErrorDecoder.java
@@ -0,0 +1,24 @@
+package com.clientui.clientui.exceptions;
+
+import feign.Response;
+import feign.codec.ErrorDecoder;
+
+
+public class CustomErrorDecoder implements ErrorDecoder {
+
+    private final ErrorDecoder defaultErrorDecoder = new Default();
+
+    @Override
+    public Exception decode(String invoqueur, Response reponse) {
+
+        if(reponse.status() == 400 ) {
+            return new ProductBadRequestException(
+                    "Requête incorrecte "
+            );
+        }
+
+
+        return defaultErrorDecoder.decode(invoqueur, reponse);
+    }
+
+}
